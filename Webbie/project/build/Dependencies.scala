d74514f80a54b3ea72d5ff85e9fa34774bcc93cf
Robin Walsh <robin@wordnik.com>
2011-04-20 09:23:53 -0700

    removing all that webbie crap

--- Webbie/project/build/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-class WebbieProject(info: ProjectInfo) extends DefaultProject(info)
-{
-      val http = "org.apache.httpcomponents" % "httpclient" % "4.1.+"
-      //val http2 = "org.apache.httpcomponents" % "httpasyncclient" % "4.0x.+"
-
-}
