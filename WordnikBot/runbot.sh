913a6df4c8b3ede8e3bd617e03504cd3cd5bfa47
Robin Walsh <robin@wordnik.com>
2011-05-09 14:27:03 -0700

    first of all: adding a runbot.sh shell script to more easily start the bot.
    secondly:
    why does sucks-rocks not work?

---
+++ WordnikBot/runbot.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SCALA=$( which scala )
+
+if [ -z ${SCALA} ]; then
+    echo "You don't seem to have scala in your path. You may wanna fix that."
+    exit 1
+fi
+
+CWD=$( pwd )
+
+${SCALA} -cp ${CWD}/lib_managed/scala_2.8.1/compile/*:${CWD}/target/scala_2.8.1/* com.wordnik.irc.ScalaBot $@
