9ff98c056c37e8a6575fcc8ed51e470864869fd6
Robin Walsh <robin@wordnik.com>
2011-09-14 16:37:15 -0700

    oops again

--- WordnikBot/src/main/scala/com/wordnik/irc/plugins/AddressPlugin.scala
+++ WordnikBot/src/main/scala/com/wordnik/irc/plugins/AddressPlugin.scala
@@ -3,15 +3,14 @@ package com.wordnik.irc.plugins
 class AddressPlugin extends GenericPlugin {
   val r = new scala.util.Random
 
-  val address = List("195 E 4th Ave. (2nd floor)", "San Mateo, CA 94401")
+  val address = "195 E 4th Ave. (2nd floor)", "San Mateo, CA 94401"
+
   val adj = List( "artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty")
-
   val adj2 = List( "base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten")
-
   val noun = List( "apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlet", "vassal", "whey-face", "wagtail")
+  val insult = "...thou " + r.shuffle(adj).head + ", " + r.shuffle(adj2).head + " " + r.shuffle(noun).head + "!"
 
   override def process(args:List[String]): List[String] = {
-    val insult = "...thou " + r.shuffle(adj).head + ", " + r.shuffle(adj2).head + " " + r.shuffle(noun).head + "!"
     List(address, insult)
   }
 
